38e7484af04a6bdf9f1bb410a6dd312c4f4c5498
Alefcastro <alefsuper@gmail.com>
2025-03-11 02:15:25 +0000

    primeira CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,7 +13,7 @@ int main() {
     char codigodacarta1, codigodacarta2 [50];
     char nomedacidade1, nomedacidade2 [50];
     int populacao1, populacao2;
-    float areakm²1, areakm²2;
+    float areakm1, areakm2;
     float PIB1, PIB2;
     int numerodepontosturisticos1, numerodepontosturisticos2;
     
@@ -30,7 +30,7 @@ int main() {
     scanf("%d", &populacao1);
 
     printf("Digite a area 1 em km²:\n");
-    scanf("%f", &areakm²1);
+    scanf("%f", &areakm1);
 
     printf("Digite o PIB 1:\n");
     scanf("%f", &PIB1);
@@ -38,12 +38,11 @@ int main() {
     printf("Digite o numero de pontos turisticos 1:\n");
     scanf("%d", &numerodepontosturisticos1);
 
-    printf("Estado 1:\n");
-    printf("Nome do Estado: %s\n", estado1);
+    printf("Nome do Estado : %s\n", estado1);
     printf("Codigo da carta: %s\n", codigodacarta1);
     printf("Nome da Cidade: %s\n", nomedacidade1);
     printf("Populacao: %d\n", populacao1);
-    printf("Area em km²: %f\n", areakm²1);
+    printf("Area em km²: %f\n", areakm1);
     printf("PIB: %f\n", PIB1);
     printf("Numero de pontos turisticos: %d\n", numerodepontosturisticos1);
 
