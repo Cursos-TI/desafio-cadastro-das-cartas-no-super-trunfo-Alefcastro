6697d93155db9d338da5e0c02475f14889784ed2
Alefcastro <alefsuper@gmail.com>
2025-03-11 01:59:48 +0000

    iniciei o trabalho

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,53 @@
 //Teste larissa
 
 int main() {
-    printf("Desafio super trunfo - Paises. \n");
+
+
+    char estado1, estado2 [50];
+    char codigodacarta1, codigodacarta2 [50];
+    char nomedacidade1, nomedacidade2 [50];
+    int populacao1, populacao2;
+    float areakm²1, areakm²2;
+    float PIB1, PIB2;
+    int numerodepontosturisticos1, numerodepontosturisticos2;
+    
+    printf("Digite o estado 1: \n");
+    scanf("%s", &estado1);
+
+    printf("Digite o codigo da carta1: \n");
+    scanf("%s", &codigodacarta1);
+
+    printf("Digite o nome da cidade 1: \n");
+    scanf("%s", &nomedacidade1);
+
+    printf("Digite a populacao da cidade 1: \n");
+    scanf("%d", &populacao1);
+
+    printf("Digite a area 1 em km²:\n");
+    scanf("%f", &areakm²1);
+
+    printf("Digite o PIB 1:\n");
+    scanf("%f", &PIB1);
+
+    printf("Digite o numero de pontos turisticos 1:\n");
+    scanf("%d", &numerodepontosturisticos1);
+
+    printf("Estado 1:\n");
+    printf("Nome do Estado: %s\n", estado1);
+    printf("Codigo da carta: %s\n", codigodacarta1);
+    printf("Nome da Cidade: %s\n", nomedacidade1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area em km²: %f\n", areakm²1);
+    printf("PIB: %f\n", PIB1);
+    printf("Numero de pontos turisticos: %d\n", numerodepontosturisticos1);
+
+   
+   
+    
+
+
+
+
 
 
     
